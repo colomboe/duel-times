96021297ed15ecab2bf4e140966de205198d75f8
emacolombo <emanuele.colombo@lastminute.com>
2024-08-12 16:22:45 +0200

    Media converted to require less bandwidth. Added initial progress bar for resource fetching.

--- src/game/StartScene.ts
+++ src/game/StartScene.ts
@@ -5,15 +5,19 @@ export class StartScene extends Phaser.Scene {
 
     preload() {
 
-        const progress = this.add.graphics();
+        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+        const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+
+        const progressBarBackground = this.add.rectangle(screenCenterX - 400, screenCenterY, 800, 60, 0x777777, 1).setOrigin(0, 0);
+        const progressBarValue = this.add.rectangle(screenCenterX - 400, screenCenterY, 0, 60, 0x0EC756, 1).setOrigin(0, 0);
+
         this.load.on('progress', (value: number) => {
-            progress.clear();
-            progress.fillStyle(0xffffff, 1);
-            progress.fillRect(0, 270, 800 * value, 60);
+            progressBarValue.setSize(value * 800, 60);
         });
 
         this.load.on('complete', () => {
-            progress.destroy();
+            progressBarBackground.destroy();
+            progressBarValue.destroy();
         });
 
         this.load.image('title', 'game-assets/title.png');
@@ -23,19 +27,19 @@ export class StartScene extends Phaser.Scene {
         this.load.audio('menu-sfx', 'game-assets/sfx/menu.mp3');
         this.load.audio('counter-sfx', 'game-assets/sfx/counter.mp3');
 
-        this.load.image('rival-frame', 'game-assets/rivals/current.png');
-        this.load.image('rival-placeholder', 'game-assets/rivals/placeholder.png');
+        this.load.image('rival-frame', 'game-assets/rivals/current.jpeg');
+        this.load.image('rival-placeholder', 'game-assets/rivals/placeholder.jpeg');
         this.load.image('sparkle', 'game-assets/sparkle-red.png');
 
         players.forEach(player => {
-            this.load.image(`player-${player.id}`, `game-assets/players/${player.id}/normal.png`);
-            this.load.image(`player-defeated-${player.id}`, `game-assets/players/${player.id}/crying.png`);
-            this.load.image(`player-winner-${player.id}`, `game-assets/players/${player.id}/happy.png`);
+            this.load.image(`player-${player.id}`, `game-assets/players/${player.id}-normal.jpeg`);
+            this.load.image(`player-defeated-${player.id}`, `game-assets/players/${player.id}-crying.jpeg`);
+            this.load.image(`player-winner-${player.id}`, `game-assets/players/${player.id}-happy.jpeg`);
         });
         gameStatus.levels.forEach(level => {
-            this.load.image(`background-${level.id}`, `game-assets/backgrounds/${level.background}.png`);
-            this.load.image(`rival-${level.id}`, `game-assets/rivals/${level.rivalAvatar}.png`);
-            this.load.image(`rival-defeated-${level.id}`, `game-assets/rivals/${level.rivalAvatar}-defeated.png`);
+            this.load.image(`background-${level.id}`, `game-assets/backgrounds/${level.background}.jpeg`);
+            this.load.image(`rival-${level.id}`, `game-assets/rivals/${level.rivalAvatar}.jpeg`);
+            this.load.image(`rival-defeated-${level.id}`, `game-assets/rivals/${level.rivalAvatar}-defeated.jpeg`);
         });
 
     }
