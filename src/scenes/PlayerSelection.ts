1e969cdaedfd71476c8f3d0b82a56e0cceb692e6
emacolombo <emanuele.colombo@lastminute.com>
2024-08-30 21:09:54 +0200

    WIP refactoring to make the scenes code understandable...

--- src/scenes/PlayerSelection.ts
+++ src/scenes/PlayerSelection.ts
@@ -1,76 +1,69 @@
 import Image = Phaser.GameObjects.Image;
+import Text = Phaser.GameObjects.Text;
 
 import {Player} from "../model/definitions.ts";
 import {gameStatus, players} from "../model/data.ts";
 import {dictionary} from "../model/i18n.ts";
+import {BaseScene} from "./BaseScene.ts";
+import {paletteString, timing} from "../Config.ts";
 
-export class PlayerSelection extends Phaser.Scene {
+export class PlayerSelection extends BaseScene {
 
-    private avatars: Phaser.GameObjects.Image[] = [];
-    private names: Phaser.GameObjects.Text[] = [];
+    private avatars: Image[] = [];
+    private names: Text[] = [];
 
     preload() {
     }
 
     create() {
-        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+        const center = this.screenCenter();
         const offsetX = this.cameras.main.worldView.x + this.cameras.main.width / (players.length + 1);
 
         players.forEach((p, index) => {
             const avatar = this.createAvatar(offsetX, index, p);
-            this.avatars.push(avatar);
-            avatar.once("pointerdown", () => {
-                gameStatus.selectedPlayer = p;
-                this.sound.add("menu-sfx", { loop: false, volume: 1 }).play();
-                this.names.forEach(t => {
-                    this.tweens.add({
-                        targets: t,
-                        duration: 1000,
-                        alpha: 0,
-                    }).play();
-                });
-                this.avatars.forEach(a => {
-                    if (a !== avatar)
-                        this.tweens.add({
-                            targets: a,
-                            duration: 1000,
-                            alpha: 0,
-                        }).play();
-                });
-
-                setTimeout(() => this.cameras.main.fadeOut(1000, 0, 0, 0), 1500);
-            });
+            avatar.once("pointerdown", () => this.avatarSelected(avatar, p));
         });
 
-        const pressToStartText = this.add.text(
-            screenCenterX,
-            150,
-            dictionary.selectPlayer,
-            {fontFamily: "Arial Black, Arial-BoldMT", fontSize: 74, color: "#ccffff"}
-        );
-        pressToStartText.setStroke("#336699", 16);
-        pressToStartText.setShadow(2, 2, "#333333", 2, true, false);
-        pressToStartText.setOrigin(0.5);
+        this.add.text(center.x, 150, dictionary.selectPlayer,
+            {fontFamily: "Arial Black, Arial-BoldMT", fontSize: 74, color: paletteString.lightCyan})
+            .setStroke(paletteString.blue, 16)
+            .setShadow(2, 2, paletteString.darkGray, 2, true, false)
+            .setOrigin(0.5);
 
-        this.cameras.main.fadeIn(1000, 0, 0, 0);
-
-        this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
-            this.scene.start("Story");
-        });
+        this.fadeInAndThen(() => {});
     }
 
-    createAvatar(offsetX: number, index: number, player: Player): Image {
-        const avatar = this.add.image(offsetX * (index + 1), 500, `player-${player.id}`);
-        avatar.setScale(0.35);
-        avatar.setInteractive();
+    private avatarSelected(avatar: Image, player: Player) {
+        gameStatus.selectedPlayer = player;
+        this.sound.add("menu-sfx", { loop: false, volume: 1 }).play();
+
+        this.names.forEach(t => {
+            this.tweens.add({ targets: t, duration: timing.fastTransition, alpha: 0 }).play();
+        });
+
+        this.avatars.forEach(a => {
+            if (a !== avatar)
+                this.tweens.add({ targets: a, duration: timing.fastTransition, alpha: 0 }).play();
+        });
+
+        setTimeout(() => this.fadeOutAndNavigateTo("Story"), timing.fastTransition * 1.5);
+    }
+
+    private createAvatar(offsetX: number, index: number, player: Player): Image {
+        const avatar = this.add.image(offsetX * (index + 1), 500, `player-${player.id}`)
+            .setScale(0.35)
+            .setInteractive();
+
         const text = this.add.text(
             offsetX * (index + 1),
             800,
             player.label,
-            {fontFamily: "Arial Black, Arial-BoldMT", fontSize: 56, color: "#ccffff"}
-        );
-        text.setOrigin(0.5);
+            {fontFamily: "Arial Black, Arial-BoldMT", fontSize: 56, color: paletteString.lightCyan}
+        ).setOrigin(0.5);
+
+        this.avatars.push(avatar);
         this.names.push(text);
+
         return avatar;
     }
 }
